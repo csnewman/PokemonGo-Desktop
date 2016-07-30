5e5a441b1ba6654ec419f47053b460573c9342c4
Chandler Newman <chandler2newman@hotmail.co.uk>
2016-07-30 22:57:05 +0100

    Added OpenStreetMap + Added coins & stardust icons to sidebar + Fixed sidebar spacing * WASD and joystick movement is supported * Currently the speed is set at 10m/s, need to add an option

---
+++ Assets/PGODesktop/UI/Map.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using RestSharp;
+using SimpleCoroutines;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace PGODesktop.UI
+{
+    public class Map : UIBehaviour
+    {
+        private const double TileSize = 256f;
+        private const double EarthRadius = 6378137.0;
+        public RectTransform ContentRectTransform;
+        public RectTransform PlayerArrowTransform;
+        public Texture LoadingTexture;
+        private IRestClient _restClient;
+        private IDictionary<string, Texture> _textures;
+        private RectTransform _transform;
+        private double _longitude, _latitude;
+        private bool _regenerateArea;
+        private GameObject[,] _objects;
+        private int[,,] _tiles;
+        private string[,] _currentTextures;
+        private int _gridWidth, _gridHeight;
+        private int _centerX, _centerY;
+        private int _halfWidth, _halfHeight;
+        private int _zoom;
+        private int _mapSize;
+
+        protected override void Awake()
+        {
+            _textures = new Dictionary<string, Texture>();
+            _transform = GetComponent<RectTransform>();
+            _restClient = new RestClient("http://a.tile.openstreetmap.org/");
+            MarkForRegeneration();
+            SetPosition(-0.1278f, 51.5074f);
+            SetZoom(17);
+        }
+
+        public void SetZoom(int level)
+        {
+            _zoom = level.Bound(0, 19);
+            _mapSize = 1 << _zoom;
+            _centerX = _longitude.ToTileX(_zoom).FloorToInt();
+            _centerY = _latitude.ToTileY(_zoom).FloorToInt();
+        }
+
+        public void SetPosition(float longitude, float latitude)
+        {
+            _longitude = longitude;
+            _latitude = latitude;
+        }
+
+        public void MarkForRegeneration()
+        {
+            _regenerateArea = true;
+        }
+
+        private void Update()
+        {
+            Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            if (input.magnitude > 1f)
+            {
+                input = input.normalized;
+            }
+            float distance = Vector2.Distance(Vector2.zero, input);
+            float angle = Vector2.Angle(Vector2.up, input);
+            if (input.x < 0)
+            {
+                angle = -angle;
+            }
+
+            if (distance > 0.1d)
+            {
+                PlayerArrowTransform.rotation = Quaternion.Euler(0, 0, -angle);
+            }
+            
+            double latRads = _latitude.ToRadians();
+            double lonRads = _longitude.ToRadians();
+            double angularDistance = (10d*distance*Time.deltaTime)/EarthRadius;
+            double angleRads = ((double) angle).ToRadians();
+
+            double lat = Math.Asin(
+                Math.Sin(latRads)*Math.Cos(angularDistance) +
+                Math.Cos(latRads)*Math.Sin(angularDistance)*Math.Cos(angleRads));
+
+            double lon = ((lonRads + (Math.Atan2(
+                Math.Sin(angleRads)*Math.Sin(angularDistance)*Math.Cos(latRads),
+                Math.Cos(angularDistance) - Math.Sin(latRads)*Math.Sin(lat))) + Math.PI)%Utils.TwoPi) - Math.PI;
+
+            _latitude = lat.ToDegrees();
+            _longitude = lon.ToDegrees();
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SetZoom(_zoom + 1);
+            }
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                SetZoom(_zoom - 1);
+            }
+
+            double offsetX = _longitude.ToTileX(_zoom) - (_centerX + 0.5d);
+            double offsetY = _latitude.ToTileY(_zoom) - (_centerY + 0.5d);
+
+            if (offsetX.Abs() > 1d)
+            {
+                int change = offsetX.MinInt();
+                _centerX += change;
+                offsetX -= change;
+            }
+
+            if (offsetY.Abs() > 1d)
+            {
+                int change = offsetY.MinInt();
+                _centerY += change;
+                offsetY -= change;
+            }
+
+            ContentRectTransform.localPosition = new Vector3((float) (-offsetX*TileSize).Round(0),
+                (float) (offsetY*TileSize).Round(0), 0);
+
+            if (_regenerateArea)
+            {
+                _regenerateArea = false;
+                if (_objects != null)
+                {
+                    foreach (GameObject go in _objects)
+                    {
+                        Destroy(go);
+                    }
+                }
+
+                _halfHeight = (_transform.rect.height/TileSize/2d).CeilToInt() + 1;
+                _halfWidth = (_transform.rect.width/TileSize/2d).CeilToInt() + 1;
+                _gridHeight = _halfHeight*2 + 1;
+                _gridWidth = _halfWidth*2 + 1;
+                _objects = new GameObject[_gridWidth, _gridHeight];
+                _tiles = new int[_gridWidth, _gridHeight, 3];
+                _currentTextures = new string[_gridWidth, _gridHeight];
+
+                for (int x = 0; x < _gridWidth; x++)
+                {
+                    for (int y = 0; y < _gridHeight; y++)
+                    {
+                        GameObject go = new GameObject();
+                        RectTransform transform = go.AddComponent<RectTransform>();
+                        transform.SetParent(ContentRectTransform, false);
+                        transform.localScale = new Vector3(1, 1, 1);
+                        transform.anchorMax = new Vector2(0.5f, 0.5f);
+                        transform.anchorMin = new Vector2(0.5f, 0.5f);
+                        transform.sizeDelta = new Vector2((float) TileSize, (float) TileSize);
+                        transform.localPosition = new Vector3((float) ((-_halfWidth + x)*TileSize),
+                            (float) ((_halfHeight - y)*TileSize), 0);
+                        transform.name = "Cell_" + x + "_" + y;
+                        _objects[x, y] = go;
+                    }
+                }
+            }
+
+
+            if (_objects != null)
+            {
+                for (int x = 0; x < _gridWidth; x++)
+                {
+                    for (int y = 0; y < _gridHeight; y++)
+                    {
+                        int rx = _centerX + (-_halfWidth + x);
+                        int ry = _centerY + (-_halfHeight + y);
+                        string key = _zoom + "/" + rx.Wrap(_mapSize) + "/" + ry.Wrap(_mapSize);
+
+                        if (_currentTextures[x, y] != key)
+                        {
+                            if (_textures.ContainsKey(key))
+                            {
+                                _objects[x, y].GetOrAddComponent<RawImage>().texture = _textures[key];
+                                _currentTextures[x, y] = key;
+                            }
+                            else
+                            {
+                                _objects[x, y].GetOrAddComponent<RawImage>().texture = LoadingTexture;
+
+                                if (_tiles[x, y, 0] != rx || _tiles[x, y, 1] != ry || _tiles[x, y, 2] != _zoom)
+                                {
+                                    _tiles[x, y, 0] = rx;
+                                    _tiles[x, y, 1] = ry;
+                                    _tiles[x, y, 2] = _zoom;
+                                    CoroutineManager.Start("load_" + UnityEngine.Random.value,
+                                        LoadTile(key));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private IEnumerator LoadTile(string key)
+        {
+            yield return new EnterBackground();
+
+            IRestResponse response = _restClient.Execute(new RestRequest(key + ".png", Method.GET));
+
+            yield return new EnterForeground();
+
+            Texture2D texture2D = new Texture2D(256, 256, TextureFormat.ARGB32, false);
+            texture2D.LoadImage(response.RawBytes);
+            _textures[key] = texture2D;
+        }
+
+        protected override void OnRectTransformDimensionsChange()
+        {
+            _regenerateArea = true;
+        }
+    }
+}
--- Assets/PGODesktop/Utils.cs
+++ Assets/PGODesktop/Utils.cs
@@ -4,11 +4,15 @@ using Google.Protobuf;
 using POGOProtos.Networking.Envelopes;
 using POGOProtos.Networking.Requests;
 using RestSharp;
+using UnityEngine;
+using Random = System.Random;
 
 namespace PGODesktop
 {
     public static class Utils
     {
+        public const double TwoPi = Math.PI*2;
+
         public static string GetHeader(this IRestResponse response, string name)
         {
             String value = null;
@@ -92,6 +96,132 @@ namespace PGODesktop
             T t = new T();
             t.MergeFrom(envelope.Returns[index]);
             return t;
+        }
+
+        public static double ToRadians(this double val)
+        {
+            return 0.0174532925199433D*val;
+        }
+
+        public static double ToDegrees(this double val)
+        {
+            return val*57.2957795130823D;
+        }
+
+        public static float ToTileX(this float val, int zoom)
+        {
+            return (float) ((val + 180.0d)/360.0d*(1 << zoom));
+        }
+
+        public static double ToTileX(this double val, int zoom)
+        {
+            return (val + 180.0d)/360.0d*(1 << zoom);
+        }
+
+        public static float ToTileY(this float val, int zoom)
+        {
+            return (float) ((1.0d - Math.Log(Math.Tan(val*Math.PI/180.0d) +
+                                             1.0d/Math.Cos(val*Math.PI/180.0d))/Math.PI)/2.0d*(1 << zoom));
+        }
+
+        public static double ToTileY(this double val, int zoom)
+        {
+            return (1.0d - Math.Log(Math.Tan(val*Math.PI/180.0d) +
+                                    1.0d/Math.Cos(val*Math.PI/180.0d))/Math.PI)/2.0d*(1 << zoom);
+        }
+
+        public static float ToLongitude(this float val, int zoom)
+        {
+            return (float) ((val/Math.Pow(2.0, zoom)*360.0) - 180.0);
+        }
+
+        public static float ToLatitude(this float val, int zoom)
+        {
+            double n = Math.PI - ((2.0*Math.PI*val)/Math.Pow(2.0, zoom));
+            return (float) (180.0/Math.PI*Math.Atan(Math.Sinh(n)));
+        }
+
+        public static int FloorToInt(this float val)
+        {
+            return Mathf.FloorToInt(val);
+        }
+
+        public static int FloorToInt(this double val)
+        {
+            return (int) Math.Floor(val);
+        }
+
+        public static int CeilToInt(this float val)
+        {
+            return Mathf.CeilToInt(val);
+        }
+
+        public static int CeilToInt(this double val)
+        {
+            return (int) Math.Ceiling(val);
+        }
+
+        public static float Abs(this float val)
+        {
+            return Mathf.Abs(val);
+        }
+
+        public static double Abs(this double val)
+        {
+            return Math.Abs(val);
+        }
+
+        public static int MinInt(this float val)
+        {
+            return val < 0 ? val.CeilToInt() : val.FloorToInt();
+        }
+
+        public static int MinInt(this double val)
+        {
+            return val < 0 ? val.CeilToInt() : val.FloorToInt();
+        }
+
+        public static T GetOrAddComponent<T>(this GameObject go) where T : Component
+        {
+            T t = go.GetComponent<T>();
+            if (t != null)
+            {
+                return t;
+            }
+            return go.AddComponent<T>();
+        }
+
+        public static int Bound(this int val, int min, int max)
+        {
+            if (val < min)
+            {
+                val = min;
+            }
+
+            if (val > max)
+            {
+                val = max;
+            }
+
+            return val;
+        }
+
+        public static int Pos(this int val)
+        {
+            if (val < 0)
+                return -val;
+            return val;
+        }
+
+        public static int Wrap(this int val, int range)
+        {
+            val %= range;
+            return val < 0 ? range + val : val;
+        }
+
+        public static double Round(this double val, int points)
+        {
+            return Math.Round(val, points, MidpointRounding.AwayFromZero);
         }
     }
 }
